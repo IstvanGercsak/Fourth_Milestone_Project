18aa1ee45e48b82f5a61b4a296a23249de8bb850
istvan.gercsak <istvan.gercsak@gmail.com>
2019-10-24 13:56:07 +0100

    Create QA Database

--- Fourth_Milestone_Project/settings.py
+++ Fourth_Milestone_Project/settings.py
@@ -82,17 +82,7 @@ WSGI_APPLICATION = 'Fourth_Milestone_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if os.environ.get('DEVELOPMENT'):
-    print("Start Database in DEV mod")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    print("Start Database in PROD mod")
-    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
