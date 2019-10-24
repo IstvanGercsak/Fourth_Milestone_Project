2761068227858a0fed8f6ff668634724f965e2a3
istvan.gercsak <istvan.gercsak@gmail.com>
2019-10-24 15:37:57 +0100

    Index page and first test

--- Fourth_Milestone_Project/settings.py
+++ Fourth_Milestone_Project/settings.py
@@ -13,13 +13,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.path.exists('env.py'):
+    import env
+
 if os.environ.get('DEVELOPMENT'):
     development = True
 else:
     development = False
-
-if os.path.exists('env.py'):
-    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
