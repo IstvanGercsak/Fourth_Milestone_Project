4ab59b94f61f82b2d6ef8fae07ae92be34788e23
istvan.gercsak <istvan.gercsak@gmail.com>
2019-10-24 16:36:51 +0100

    Add django forms bootstrap for settings

--- Fourth_Milestone_Project/settings.py
+++ Fourth_Milestone_Project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
     'home',
     'cart',
     'accounts'
@@ -131,7 +132,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_LOCATION = "static"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
