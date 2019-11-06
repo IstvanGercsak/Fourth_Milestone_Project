d8c1e3dabd6e186c4416b60f46b81d9977141ee2
Geripii <33180486+Geripii@users.noreply.github.com>
2019-11-06 18:42:18 +0000

    install new boto

--- Fourth_Milestone_Project/settings.py
+++ Fourth_Milestone_Project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 ####################
 # Css problema a dev igen/nem-el
+# Css problema a dev igen/nem-el
 ####################
 import os
 import dj_database_url
