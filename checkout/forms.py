31fe963e6caf40562d94927bf6cc446e66df1150
Geripii <33180486+Geripii@users.noreply.github.com>
2019-12-24 22:56:32 +0000

    Update button positions. Payment update.

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,10 +7,14 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
 
     credit_card_number = forms.IntegerField(label='Credit card number', required=True)
-    cvv = forms.IntegerField(label='Security code (CVV)', required=False)
+    cvv = forms.IntegerField(label='Security code (CVV)', required=True, max_value=999)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+    def clean_credit_card_number(self):
+        credit_card_number = self.cleaned_data.get("credit_card_number")
+        return self.clean_credit_card_number()
 
 
 class OrderForm(forms.ModelForm):
